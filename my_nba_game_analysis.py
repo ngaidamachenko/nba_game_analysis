024eec84efc6925459040f95e18605b45a5e239b
nikita gaidamachenko <gaidamac_n@qwasar.io>
2023-08-31 01:07:02 +0000

    trying to work out regex for names

--- my_nba_game_analysis.py
+++ my_nba_game_analysis.py
@@ -27,20 +27,33 @@ def analyse_nba_game(play_by_play_moves):
         AWAY_SCORE = play[5]
         HOME_SCORE = play[6]
         DESCRIPTION = play[7]
-        print(AWAY_TEAM)
-
-    game_summary = {"home_team": {"name": HOME_TEAM, "players_data": DATA}, "away_team": {"name": AWAY_TEAM, "players_data": DATA}}
+      #  print(AWAY_TEAM)
+        main_player_match = re.search(r'(\b[A-Z]\W\s[A-Z][a-z]+\b)', DESCRIPTION)#((\b[A-Z]\W\s[A-Z][a-z]+\b)\s)|
+        if main_player_match:
+            main_player = main_player_match[0]
+        else:
+            continue
+        print(main_player)
+    #game_summary = {"home_team": {"name": HOME_TEAM, "players_data": DATA}, "away_team": {"name": AWAY_TEAM, "players_data": DATA}}
     #DATA will be an array of hashes with this format:
-    DATA = {"player_name": XXX, "FG": XXX, "FGA": XXX, "FG%": XXX, "3P": XXX, "3PA": XXX, "3P%": XXX, "FT": XXX, "FTA": XXX, "FT%": XXX, "ORB": XXX, "DRB": XXX, "TRB": XXX, "AST": XXX, "STL": XXX, "BLK": XXX, "TOV": XXX, "PF": XXX, "PTS": XXX}
-    DATA_HEADER = "\t".join(["player_name", "FG", "FGA", "FG%", "3P", "3PA", "3P%", "FT", "FTA", "FT%", "ORB", "DRB", "TRB", "AST", "STL", "BLK", "TOV", "PF", "PTS"])
+   #DUNE - PART II
+    #PLAYER_DATA = {"player_name": XXX, "FG": XXX, "FGA": XXX, "FG%": XXX, "3P": XXX, "3PA": XXX, "3P%": XXX, "FT": XXX, "FTA": XXX, "FT%": XXX, "ORB": XXX, "DRB": XXX, "TRB": XXX, "AST": XXX, "STL": XXX, "BLK": XXX, "TOV": XXX, "PF": XXX, "PTS": XXX}
+    #DATA_HEADER = "\t".join(["player_name", "FG", "FGA", "FG%", "3P", "3PA", "3P%", "FT", "FTA", "FT%", "ORB", "DRB", "TRB", "AST", "STL", "BLK", "TOV", "PF", "PTS"])
     #return game_summary
+    #DATA = DATA_HEADER.append(DATA)
+    #regex_main_name = #regex mainname = ((\b[A-Z]\W\s[A-Z][a-z]+\b)\s)|(\b[A-Z]\W\s[A-Z][a-z]+\b)\( to search for a name followed by spacebar or '(' - main player's name
+                       #regex supportname = (\b[A-Z]\W\s[A-Z][a-z]+\b)\)
+   # regex_fg
+   # regex_fga
+    
+
 def _main():
     play_by_play_moves = load_data("nba_game_blazers_lakers_20181018.txt")
     analyse_nba_game(play_by_play_moves)
 
 _main()
 
-#DUNE - PART II
+
 
 """
     FG: Field Goals Made
